8fdaed145f0ec41d697b9c056cc4c72e75b126dc
Ric Worstell <87674474+DeathMyst@users.noreply.github.com>
2022-09-29 22:43:29 -0700

    Initial commit

---
+++ src/components/instructions.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+<div align="center">
+    <h1>Instructions to Play</h1>
+    <ol>
+        <li>The name of the game is BlackJack or 21.</li>
+        <li>The object of the game is to get a BlackJack, which is an ace (A) and a face card (K, Q, J) or a 10, to
+            equal 21 or to get as close to 21 as you can WITHOUT going over 21.</li>
+        <li>The Ace (A) card counts as either 1 or as 11. So if your delt an Ace (A) and say a 5, that totals either 6
+            or 16.</li>
+        <li>If the Dealer is delt an Ace (A) face up, you can take insurance on your bet, which is one half your bet. If
+            the dealer does have 21 or blackjack, you keep your bet and the insurance you posted, if the dealer does not
+            have blackjack then you lose just the insurance you posted and keep your bet to finish playing the hand out.
+        </li>
+    </ol>
+
+    <ol>
+        <li>Click Deal to deal your hand.</li>
+        <li>Once you have your hand decide if you want to HIT (take another card) or STAND (just play the cards you
+            have.</li>
+        <li>Total your cards and HIT until your happy with what you have OR until you Bust (go over 21)</li>
+        <li>If you get a BlackJack, and Ace and a 10 value card (K, Q, J, 10) and the dealer does not get a BlackJack,
+            you will be paid immediately and the hand is over.</li>
+    </ol>
+</div>
+
+</html >
